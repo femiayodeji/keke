d0f3b78897304cd5c24323a311d00844b8117e41
Femi Ayodeji <femidotayodeji@gmail.com>
2021-03-20 00:12:22 +0100

    Create noise using linear interpolation

--- src/index.js
+++ src/index.js
@@ -1,14 +1,33 @@
-const canvas = document.createElement("canvas");
+var canvas = document.createElement("canvas");
 let context = canvas.getContext("2d");
 canvas.width = 350;
 canvas.height = 500;
 
 document.body.appendChild(canvas);
 
+var perm = [];
+
+while(perm.length < 255){
+    let val = Math.floor(Math.random() * 255);
+    perm.push(val);
+}
+console.log(perm)
+var lerp = (a, b, t) => a + (b - a) * t;
+var noise = x => {
+    return lerp(perm[Math.floor(x)], perm[Math.ceil(x)], x - Math.floor(x));
+}
+
 function gameLoop(){
     context.fillStyle = "#19f";
     context.fillRect(0, 0, canvas.width, canvas.height);
 
+    context.fillStyle = "#000";
+    context.beginPath();
+
+    for(let i = 0; i < canvas.width; i++){
+        context.lineTo(i, noise(i));
+    }
+    context.fill();
     requestAnimationFrame(gameLoop);
 }
 
